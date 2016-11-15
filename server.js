4c4e408e33b0497c10887125c5f345968132f171
Satyrisk <leofotoblogg@gmail.com>
2016-11-15 16:49:18 +0100

    Oppdatert

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
+'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const tvShowruter = require('./tvShow/tvShowRouter');
-
+const tvShowRouter = require('./tvShow/tvShowRouter');
+const reviewRouter = require('./review/reviewRouter');
 
 const app = express();
 const APP_PORT = 3000;
 
-
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.get('/hello', (req, res) => { res.send('Hello World')});
+// Exercise #1
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+});
 
+// Exercise #2
 app.use('/tvshow', tvShowRouter);
+// Exercise #3
+app.use('/review', reviewRouter);
+
 app.listen(APP_PORT, () => {
-  console.log('App running on port ${APP_PORT}');
+  console.log(`App running on port ${APP_PORT}`);
 });
